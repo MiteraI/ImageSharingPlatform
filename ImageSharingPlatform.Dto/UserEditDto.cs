f6a21242449b8160d4a50cf1500e77d9c1aadf2f
MiteraI <91677179+MiteraI@users.noreply.github.com>
2024-03-16 22:07:59 +0700

    Update UserEditDto.cs

--- ImageSharingPlatform.Dto/UserEditDto.cs
+++ ImageSharingPlatform.Dto/UserEditDto.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace ImageSharingPlatform.Dto
         public Guid Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
