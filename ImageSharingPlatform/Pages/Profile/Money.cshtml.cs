1f39d92bb1f5f67ef4d80bed060eee9c7993b41e
Miraitowa79 <thaouyen7923@gmail.com>
2024-03-20 21:24:08 +0700

    Update money message

--- ImageSharingPlatform/Pages/Profile/Money.cshtml.cs
+++ ImageSharingPlatform/Pages/Profile/Money.cshtml.cs
@@ -28,8 +28,8 @@ namespace ImageSharingPlatform.Pages.Profile
         {
             if (Money < 100000 || Money % 1000 != 0)
             {
-				// Show error message in view asp-validation
-                ModelState.AddModelError("Money", "The amount must be greater than 100,000 and divisible by 1,000,000");
+                // Show error message in view asp-validation
+                TempData["ErrorMessage"] = "The amount must be greater than 100,000 and divisible by 1,000,000";
 				return Page();
 			}
             // Vnpay needs the amount to divide by 100
